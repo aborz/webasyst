7cb2390b5dd8bc6b9cb2ea31c21488a8ab4682d7
bstuff <bstuff@Mac-mini-minibstf-2.local>
2015-06-16 13:49:56 +0300

    Поменял настройки на своем локальном сервере

--- wa-config/routing.php
+++ wa-config/routing.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  'localhost:8083' => 
+  'sela.dev' => 
   array (
     0 => 
     array (
@@ -59,66 +59,7 @@ return array (
       'drop_out_of_stock' => '1',
     ),
   ),
-  '10.16.0.40:8083/index.php' => 
+  'sela.dev:8083' => 
   array (
-  ),
-  'www.selashop.ru' => 
-  array (
-    0 => 
-    array (
-      'url' => 'lookbook/*',
-      'app' => 'photos',
-      '_name' => 'Look book',
-      'theme' => 'comfortbuy',
-      'theme_mobile' => 'comfortbuy',
-      'locale' => 'ru_RU',
-      'url_type' => '0',
-      'title' => '',
-      'meta_keywords' => '',
-      'meta_description' => '',
-    ),
-    1 => 
-    array (
-      'url' => 'news/*',
-      'app' => 'blog',
-      '_name' => 'Новости',
-      'theme' => 'comfortbuy',
-      'theme_mobile' => 'comfortbuy',
-      'locale' => 'ru_RU',
-      'blog_url_type' => '1',
-      'post_url_type' => '0',
-      'title_type' => 'blog_post',
-      'title' => '',
-      'meta_keywords' => '',
-      'meta_description' => '',
-      'rss_title' => '',
-    ),
-    2 => 
-    array (
-      'url' => 'site/*',
-      'app' => 'site',
-      'theme' => 'comfortbuy',
-      'theme_mobile' => 'comfortbuy',
-      'locale' => 'ru_RU',
-      'private' => '1',
-    ),
-    3 => 
-    array (
-      'url' => '*',
-      'app' => 'shop',
-      'theme' => 'comfortbuy',
-      'theme_mobile' => 'comfortbuy',
-      'locale' => 'ru_RU',
-      'title' => 'Официальный интернет-магазин SELA. Одежда, аксессуары и обувь онлайн с доставкой по России.',
-      'meta_keywords' => 'Sela, интернет-магазин sela, одежда sela, села, интернет магазин одежды, магазин одежды, одежда, селла, модная одедва',
-      'meta_description' => 'В официальном интернет-магазине одежды SELA Вас ждет широкий ассортимент модной женской, женской, детской и мужской одежды и аксессуаров. Самовывоз, курьерская доставка и почтовая доставка по России.',
-      'url_type' => '2',
-      'type_id' => '0',
-      'payment_id' => '0',
-      'shipping_id' => '0',
-      'currency' => 'RUB',
-      'stock_id' => '1',
-      'drop_out_of_stock' => '1',
-    ),
   ),
 );
