f1552726271216de48fa35cdf8a0614b73fc3948
Andrew Lavrentev <bighustler@bighustles-iMac.local>
2015-07-09 12:19:08 +0300

    Скрипт-трекер, который проси Борис

--- wa-cache/apps/webasyst/cache/app_settings/logs.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  0 => 
-  array (
-    'app_id' => 'logs',
-    'name' => 'update_time',
-    'value' => '1',
-  ),
-);
---
+++ wa-config/apps/site/domains/sela.dev.php
@@ -0,0 +1,27 @@
+<?php
+return array (
+  'personal_fields' => 
+  array (
+    'firstname' => true,
+    'email' => true,
+    'phone' => true,
+    'address' => true,
+    'birthday' => true,
+    'locale' => true,
+    'timezone' => true,
+    'about' => true,
+    'my_sizes' => true,
+    'fav_style' => true,
+    'fav_color' => true,
+    'family_friends' => true,
+    'hobbies' => true,
+    'pets' => true,
+    'heroes' => true,
+    'cloth_brands' => true,
+    'shoe_brands' => true,
+    'cosmetic_shops' => true,
+    'grocery_stores' => true,
+    'other_shops' => true,
+    'password' => true,
+  ),
+);
