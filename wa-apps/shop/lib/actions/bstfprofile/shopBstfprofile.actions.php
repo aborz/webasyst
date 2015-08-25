0c0c0b7dddaf877b1ee1efd8f140277354919f75
Andrew Lavrentev <andrew.lavrentev@gmail.com>
2015-08-25 18:51:29 +0300

    Прокладка для перенаправления запросов

---
+++ wa-apps/shop/lib/actions/bstfprofile/shopBstfprofile.actions.php
@@ -0,0 +1,11 @@
+<?php
+class shopBstfprofileActions extends waJsonActions
+{
+function selaCitiesAction()
+    {
+        $this->response = array(
+            'valA' => 'value first',
+            'valB' => 'velue second',
+        );
+    }
+}
